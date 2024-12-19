e655a6a01a9dccaf23f697f53f6bd0391d44f350
ishanrana363 <ishanrana363@gmail.com>
2024-12-19 08:35:14 +0600

    user profile find api created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,38 +6,53 @@ const bcrypt = require("bcrypt");
 const createUser = async (req, res) => {
     try {
         const reqBody = req.body;
-        const {email,password,confirmPassword} = req.body;
-        const existUser = await userModel.findOne({email : email});
-        if(existUser)return successResponse(res,409,"User already exists",existUser);
-        
-        if(password!== confirmPassword) return successResponse(res,400,"Passwords do not match",null);
+        const { email, password, confirmPassword } = req.body;
+        const existUser = await userModel.findOne({ email: email });
+        if (existUser) return successResponse(res, 409, "User already exists", existUser);
+
+        if (password !== confirmPassword) return successResponse(res, 400, "Passwords do not match", null);
 
         const user = await userModel.create(reqBody);
-        return successResponse(res,201,"User created successfully",user);
+        return successResponse(res, 201, "User created successfully", user);
 
     } catch (error) {
-        return errorResponse(res,500,"Something went wrong",error);
+        return errorResponse(res, 500, "Something went wrong", error);
     }
 };
 
 
 const loginUser = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        const user = await userModel.findOne({email: email});
-        if(!user) return errorResponse(res,404,"User not found",null);
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email: email });
+        if (!user) return errorResponse(res, 404, "User not found", null);
         let matchPassword = bcrypt.compareSync(password, user.password);;
-        if(!matchPassword) return errorResponse(res,400,"Invalid credentials",null);
-        const token = tokenCreate({user}, process.env.JWT_KEY, "10d" );
-        return successResponse(res,200,"Logged in successfully",{user,token});
+        if (!matchPassword) return errorResponse(res, 400, "Invalid credentials", null);
+        const token = tokenCreate({ user }, process.env.JWT_KEY, "10d");
+        return successResponse(res, 200, "Logged in successfully", { user, token });
     } catch (error) {
         console.log(error)
-        errorResponse(res,500,"Something went wrong",error);
+        errorResponse(res, 500, "Something went wrong", error);
+    }
+};
+
+const userProfile = async (req, res) => {
+    try {
+        const id = req.headers._id;
+        const filter = {
+            _id : id,
+        };
+        const user = await userModel.findOne(filter);
+        if (!user) return errorResponse(res, 404, "User not found", null);
+        return successResponse(res, 200, "User profile fetched successfully", user);
+    } catch (error) {
+        errorResponse(res, 500, "Something went wrong", error);
     }
 };
 
 
 module.exports = {
     createUser,
-    loginUser
+    loginUser,
+    userProfile
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const userConroller = require("../controllers/userController");
+const { isLogIn } = require('../middlewares/authMiddleware');
 
 
 router.post("/user-create", userConroller.createUser);
 router.post("/user-login", userConroller.loginUser);
+router.get("/user-profile", isLogIn ,userConroller.userProfile);
 
 
 
